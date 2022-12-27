6d6a6d5f2bd5d20511b6d91ea34ebe0b41ec8b1d
MekadAlibrahem <mekadalibrahem123@gmail.com>
2022-12-28 01:25:12 +0300

    add solution to  assignment02 week01

---
+++ assignments/week001/assignment02.cpp
@@ -0,0 +1,32 @@
+// لتكليف 02
+
+//     ما هي أسماء ال Function الأخرى الموجودة في ال iostream Header File
+//     يمكنك كتابتها في تعليق كما في المثال. تم كتابة أول واحدة لك لعرض الفكرة
+
+// /*
+//   iostream Header File Content
+//   -- 
+//   -- 
+//   -- 
+//   -- 
+//   -- 
+//   -- 
+//   -- 
+//   -- 
+// */
+
+
+// ==================================================
+// solution 
+
+/*
+  iostream Header File Content
+  -- cout
+  -- cin
+  -- cerr
+  -- clog
+  -- wcin
+  -- wcout
+  -- wcerr
+  -- wclog
+*/
