9eefc54f2a3bcebeff975d019cac1c60b9515613
MekadAlibrahem <mekadalibrahem123@gmail.com>
2022-12-28 01:25:24 +0300

    add solution to  assignment03 week01

---
+++ assignments/week001/assignment03.cpp
@@ -0,0 +1,24 @@
+// التكليف 03
+
+//     قم بطباعة المطلوب كما في المثال
+//     يجب عليك إستخدام cout مرة واحدة فقط
+
+// Iam Learning Programming
+// With
+// Elzero
+// Web
+// School
+// <3
+
+//============================================================= 
+// solution 
+
+#include <iostream> 
+
+
+int main(){
+  
+    std::cout << "Iam Learning Programming\nWith\nElzero\nWeb\nSchool\n<3";
+    return 0;
+
+}
