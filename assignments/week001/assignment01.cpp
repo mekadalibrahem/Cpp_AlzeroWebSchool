e6457963fdf1813ce25d4cbc0b225f11fdbe496b
MekadAlibrahem <mekadalibrahem123@gmail.com>
2022-12-28 01:24:50 +0300

    add solution to  assignment01 week01

--- assignments/week001/assignment01.cpp
+++ assignments/week001/assignment01.cpp
@@ -20,21 +20,24 @@
 // Line Two
 // Line Three
 
+// ===================================================================================================
+// solution 
 
+//1- char '#' befor 'inculue' 
+//2- (<iostream) without " " 
 
-// solution
-//1- char '#' befor include  
-//2-  iostream witout " "
 #include <iostream> 
-//3- [int]  befor main() 
+
+//3- (int) befor (main) 
 //4- main not Main 
 
 int main(){
-    //5- cout not out 
-    //6- << not < 
-    //7- \n not /n
-    std::cout<< "Line One\n";
-    std::cout<< "Line Two\n";
-    std::cout<< "Line Three\n";
+    //5- cout not out  
+    //6- << not <
+    //7- \n not /n 
+    std::cout << "Line One\n";
+    std::cout << "Line Two\n";
+    std::cout << "Line Three\n";
     return 0;
+
 }
