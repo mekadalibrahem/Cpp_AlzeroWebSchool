3fab019e105c9f753db4b9fc78bd7b69fba65a26
MekadAlibrahem <mekadalibrahem123@gmail.com>
2023-01-08 04:13:28 +0300

    add solution for week002 assignment01

---
+++ assignments/week002/assignment01.cpp
@@ -0,0 +1,54 @@
+/*
+
+التكليف 01
+
+    قم بتعريف المتغيرات في بداية ال Code لتظهر النتائج كما في المخرجات
+    لا تقم بالتعديل على السطرين تحت التعليق
+    السطر الثالث غير موجود ويجب أن تكتبه لتظهر جميع المخرجات كما في المثال
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your Code Here
+
+
+  // Do Not Edit The Next 2 Lines
+  cout << "My Age Is: " << age << "\n";
+  cout << period << " Years Ago My Age Was: " << age - period << "\n";
+
+  // Add The Third Line Below
+
+  return 0;
+}
+
+// Output
+
+"My Age Is: 40"
+"10 Years Ago My Age Was: 30"
+"After 10 Years My Age Will Be: 50"
+
+*/
+// ==================================================== 
+// solution 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your Code Here
+    int age = 40 ; 
+    int period = 10 ;
+    
+
+  // Do Not Edit The Next 2 Lines
+  cout << "My Age Is: " << age << "\n";
+  cout << period << " Years Ago My Age Was: " << age - period << "\n";
+
+  // Add The Third Line Below
+  cout <<"After " << period << "Years My Age Will Be: " << age + period ;
+
+  return 0;
+}
