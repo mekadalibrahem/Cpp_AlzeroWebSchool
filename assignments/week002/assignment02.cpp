9071b3cd55e72ac9fc438be299de76a4cc408e71
MekadAlibrahem <mekadalibrahem123@gmail.com>
2023-01-08 04:15:18 +0300

    add solution for week002 assignment02

---
+++ assignments/week002/assignment02.cpp
@@ -0,0 +1,38 @@
+/* 
+التكليف 02
+
+    اكتب سطر واحد فقط في بداية ال Code لتظهر النتائج كما في المخرجات
+    لا تقم بالتعديل على السطرين تحت التعليق
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your One Line Code Here
+
+  // Do Not Edit Below
+  a = 10, b = 20, c = 70;
+  cout << "Sum Of All Number Is: " << a + b + c;
+  return 0;
+}
+
+// Output
+
+"Sum Of All Number Is: 100"
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your One Line Code Here
+    int
+  // Do Not Edit Below
+  a = 10, b = 20, c = 70;
+  cout << "Sum Of All Number Is: " << a + b + c;
+  return 0;
+}
