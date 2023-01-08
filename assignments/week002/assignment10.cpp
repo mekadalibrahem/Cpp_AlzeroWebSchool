c4dd9a04c2447c19d626658f2f4660296efd1dc5
MekadAlibrahem <mekadalibrahem123@gmail.com>
2023-01-08 04:50:34 +0300

    add solution for week02 assignment10

---
+++ assignments/week002/assignment10.cpp
@@ -0,0 +1,47 @@
+/* 
+
+التكليف 10 تحدي
+
+    نريد عمل متغيريين عبارة عن المدخلات من المستخدم
+    المستخدم يقوم بإدخال رقم وبعده رقم آخر
+    المطلوب ايجاد حاصل ضرب الرقمين في بعضهم
+    شاهد المثال لترى النتيجة المطلوبة
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your Code Here
+
+  // Number One => 10
+  // Number Two => 100
+  // Needed Output 1000 [10 * 100]
+  return 0;
+}
+
+
+*/
+
+//================================================
+//=========       Solution          ==============
+//================================================
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your Code Here
+  int one , two ; 
+  cin >> one ;
+  cin >> two ; 
+  cout << one * two ; 
+
+  // Number One => 10
+  // Number Two => 100
+  // Needed Output 1000 [10 * 100]
+  return 0;
+}
