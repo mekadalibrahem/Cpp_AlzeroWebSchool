ea92f040193aa40cc7e86721250088a1ef23aadb
MekadAlibrahem <mekadalibrahem123@gmail.com>
2023-01-08 04:48:10 +0300

    add solution for week02 assignment09

---
+++ assignments/week002/assignment09.cpp
@@ -0,0 +1,38 @@
+/* 
+التكليف 09
+
+    نريد عمل متغير يقبل المدخلات من المستخدم في السطر الأول
+    بعدها نقوم بطباعة حاصل ضرب الرقم في نفسه
+    شاهد المثال والنتيجة لتفهم الفكرة
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your Code Here
+
+  // Num 10
+  // Needed Output 100 [10 * 10]
+  return 0;
+}
+
+*/
+
+//================================================
+//=========       Solution          ==============
+//================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your Code Here
+    int num = 0 ; 
+    cin >> num ;
+    cout<< num * num ;
+  // Num 10
+  // Needed Output 100 [10 * 10]
+  return 0;
+}
